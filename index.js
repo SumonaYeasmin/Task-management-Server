1323b4d4fe3d93ccf010fa57d9c4852ae6c051ac
SumonaYeasmin <sumonayeasmin570@gmail.com>
2025-02-22 12:21:44 +0600

    implement specific user all tasks functionality

--- index.js
+++ index.js
@@ -48,6 +48,15 @@ async function run() {
                 res.send(result);
             })
 
+                // Specific Users All Tasks
+        app.get('/all-task/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { userEmail: email };
+
+            const result = await tasksCollection.find(query).toArray();
+            res.send(result);
+        });
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
